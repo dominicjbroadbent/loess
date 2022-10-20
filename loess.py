d255a562b35762aa7e754b608079deb128fe6062
DJB <tt21864@bristol.ac.uk>
2022-10-20 13:00:58 +0100

    added support for interpolating between anchor points, so robust iterations can be done in the case of 1d data

--- loess.py
+++ loess.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+from bisect import bisect
 from scipy.spatial.distance import cdist
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
@@ -8,14 +10,51 @@ from kernels import rbf_kernel
 def make_poly_pred_(x, y, anchor, weights, degree):
     # Transform the data and the regression anchor into the correct basis
     x_ = PolynomialFeatures(degree).fit_transform(x)
-    anchor_ = PolynomialFeatures(degree).fit_transform(anchor.reshape(1, -1))
+    
+    if anchor.shape[0] == 1:
+        anchor = anchor.reshape(-1, 1)
+    else:
+        anchor = anchor.reshape(1, -1)
+    anchor_ = PolynomialFeatures(degree).fit_transform(anchor)
     # Fit the weighted linear regression model on the data
     model = LinearRegression().fit(x_, y, sample_weight = weights)
     
     # Predict at the regression anchor
-    y_hat = model.predict(anchor_).item()
+    y_hat = model.predict(anchor_)
     
     return y_hat
+
+def interpolate_anchors(x, y_hat, anchors):
+    y_hat_interpolated = np.zeros(x.shape[0])
+    for i in range(x.shape[0]):
+        if x[i] == anchors.min():
+            y_hat_interpolated[i] = y_hat[anchors.argmin()]
+            continue
+        if x[i] == anchors.max():
+            y_hat_interpolated[i] = y_hat[anchors.argmax()]
+            continue
+
+        # Figure out which anchor points the x is between 
+        after_idx = bisect(anchors, x[i])
+        before_idx = after_idx - 1
+
+        # Get the coords of the anchor points
+        x0 = anchors[before_idx]
+        y0 = y_hat[before_idx]
+
+        x1 = anchors[after_idx]
+        y1 = y_hat[after_idx]
+
+        # Gradient of interpolating line
+        m = ( y1 - y0 ) / ( x1 - x0 )
+
+        # Intercept of interpolating line
+        c = y1 - m * x1
+
+        # Interpolate linearly
+        y_hat_interpolated[i] = m * x[i] + c
+    
+    return y_hat_interpolated
 
 def compute_robust_weights_(y, y_hat):
     residuals = y - y_hat
@@ -24,7 +63,7 @@ def compute_robust_weights_(y, y_hat):
     return (1 - robust_weights ** 2) ** 2
 
 def fit_loess(x, y, anchors = None, degree = 2, kernel = rbf_kernel, alpha = 1, frac = None, robust_iters = 1):
-    assert degree >= 0 and isinstance(degree, int), 'robust_iters must be a non-negative integer'
+    assert degree >= 0 and isinstance(degree, int), 'degree must be a non-negative integer'
     
     if frac is not None:
         assert  0 < frac < 1, 'frac must be in (0, 1)'
@@ -33,11 +72,13 @@ def fit_loess(x, y, anchors = None, degree = 2, kernel = rbf_kernel, alpha = 1, 
     
     # Ensure arrays are 2-dimensional
     if len(x.shape) == 1:
-        x = x.reshape(-1,1)
+        x = x.reshape(-1, 1)
     
     # If we don't supply regression anchors, set every point to be one
+    anchor_interpolate_flag = True
     if anchors is None:
         anchors = x
+        anchor_interpolate_flag = False
     elif len(anchors.shape) == 1:
         anchors = anchors.reshape(-1, 1)
     
@@ -45,7 +86,7 @@ def fit_loess(x, y, anchors = None, degree = 2, kernel = rbf_kernel, alpha = 1, 
     dists = cdist(anchors, x, 'euclidean')   
     
     # Initialise robust weights as ones
-    robust_weights = np.ones(anchors.shape[0])
+    robust_weights = np.ones(y.shape[0])
     
     # Array to hold predictions for the regression anchors
     y_hat = np.zeros(anchors.shape[0])
@@ -61,7 +102,12 @@ def fit_loess(x, y, anchors = None, degree = 2, kernel = rbf_kernel, alpha = 1, 
                                            anchors[i, :], weights[i, idcs],
                                            degree)
             
-            # Stop after appropriate number of iterations
+            # Interpolate linearly between anchor points if they exist
+            # and are not just x
+            if anchor_interpolate_flag:
+                y_hat = interpolate_anchors(x, y_hat, anchors)
+                
+            # Stop recomputing robust weights after appropriate number of iterations
             if (robust_iteration + 1) != robust_iters:
                 robust_weights = compute_robust_weights_(y, y_hat)
                 
@@ -81,7 +127,12 @@ def fit_loess(x, y, anchors = None, degree = 2, kernel = rbf_kernel, alpha = 1, 
                                            anchors[i, :], weights,
                                            degree)
             
-            # Stop after appropriate number of iterations
+            # Interpolate linearly between anchor points if they exist
+            # and are not just x
+            if anchor_interpolate_flag:
+                y_hat = interpolate_anchors(x, y_hat, anchors)
+                
+            # Stop recomputing robust weights after appropriate number of iterations
             if (robust_iteration + 1) != robust_iters:
                 robust_weights = compute_robust_weights_(y, y_hat)
 
